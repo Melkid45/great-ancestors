268412c0598cc1e9a56af9293ec028fb81268480
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2024-12-11 02:19:37 +0600

    Full

--- app/js/main.js
+++ app/js/main.js
@@ -13,20 +13,53 @@ let elements = document.querySelectorAll('.element-animation');
 for (let elm of elements) {
     observer.observe(elm);
 }
+$('.header__body-burger').on('click', function(e){
+    $(this).toggleClass('active')
+    $('.header').toggleClass('active')
+    $('body').toggleClass('hidden')
+})
 
-var splide = new Splide( '#review', {
-    rewind : true,
-    pagination: false,
-    autoplay: true
-  } );
-  
-  splide.mount();
+$.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+  };
 
-var splide = new Splide( '#blog', {
-    rewind : true,
-    pagination: false,
-    autoplay: true,
-} );
-splide.mount();
+  $("input[type=tel]").click(function(){
+    $(this).setCursorPosition(3);
+  }).mask("+7(999) 999-9999");
+SmoothScroll({
+    // Время скролла 400 = 0.4 секунды
+    animationTime    : 800,
+    // Размер шага в пикселях 
+    stepSize         : 45,
+
+    // Дополнительные настройки:
+    
+    // Ускорение 
+    accelerationDelta : 30,  
+    // Максимальное ускорение
+    accelerationMax   : 1,   
+
+    // Поддержка клавиатуры
+    keyboardSupport   : true,  
+    // Шаг скролла стрелками на клавиатуре в пикселях
+    arrowScroll       : 100,
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm   : true,
+    pulseScale       : 4,
+    pulseNormalize   : 1,
+
+    // Поддержка тачпада
+    touchpadSupport   : true,
+})
 
 
